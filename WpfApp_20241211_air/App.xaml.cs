2b7374ad14fd9609fb8e7642b5980ee3068de051
owner <owner@C301-29>
2024-12-25 08:32:08 +0800

    加入專案檔案。

---
+++ WpfApp_20241211_air/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp_20241211_air
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ WpfApp_20241211_air/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_20241211_air
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string aqiURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON";
+
+        AQIdata aqiData = new AQIdata();
+        List<Field> fields = new List<Field>();
+        List<Record> records = new List<Record>();
+
+        List<Record> selectedRecords = new List<Record>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            urlTextBox.Text = aqiURL;
+        }
+
+        private async void btnGetAQI_Click(object sender, RoutedEventArgs e)
+        {
+            string url = urlTextBox.Text;
+            ContentTextBox.Text = "抓取資料中...";
+
+            string data = await GetAQIAsync(url);
+            ContentTextBox.Text = data;
+
+            aqiData = JsonSerializer.Deserialize<AQIdata>(data);
+            fields = aqiData.fields.ToList();
+            records = aqiData.records.ToList();
+            selectedRecords = records;
+            statusBarText.Text = $"共有{records.Count}筆資料";
+
+            DisplayAQIData();
+        }
+
+        private void DisplayAQIData()
+        {
+            RecordDataGrid.ItemsSource = records;
+
+            Record record = records[0];
+            DataWrapPanel.Children.Clear();
+
+            foreach (Field field in fields)
+            {
+                var propertyInfo = record.GetType().GetProperty(field.id);
+                if (propertyInfo != null)
+                {
+                    var value = propertyInfo.GetValue(record) as string;
+                    if (double.TryParse(value, out double v))
+                    {
+                        CheckBox cb = new CheckBox
+                        {
+                            Content = field.info.label,
+                            Tag = field.id,
+                            Margin = new Thickness(3),
+                            FontSize = 14,
+                            FontWeight = FontWeights.Bold,
+                            Width = 120
+                        };
+                        DataWrapPanel.Children.Add(cb);
+                    }
+                }
+            }
+        }
+
+        private async Task<string> GetAQIAsync(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+        }
+    }
+}
